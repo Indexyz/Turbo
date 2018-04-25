eea1df79da8d0dc60e90ca400240167ae5050154
Indexyz <jiduye@gmail.com>
2018-04-25 19:41:43 +0800

    feat: finish github authcation

--- src/router/auth/github.js
+++ src/router/auth/github.js
@@ -5,4 +5,16 @@ const route = router()
 
 route.get('/', passport.authenticate('github'))
 
+route.get('/callback', async ctx => {
+    try {
+        await passport.authenticate('github', {
+            successRedirect: '/',
+            failureRedirect: '/',
+        })(ctx)
+    } catch (e) {
+        console.log(e)
+        ctx.redirect('/')
+    }
+})
+
 export default route
--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,13 +1,40 @@
 import github from 'passport-github'
-import passport from 'passport'
+import passport from 'koa-passport'
 import config from 'config'
 import database from '../database/instance'
+
+passport.serializeUser(function(user, done) {
+    done(null, user.id)
+})
+
+passport.deserializeUser(async function(id, done) {
+    const user = await database('users').where('id', id)
+
+    if (user.length >= 1) {
+        return done(null, user[0])
+    } else {
+        return done(null, false)
+    }
+})
 
 const githubPassport = new github.Strategy({
     clientID: config.get('github.clientId'),
     clientSecret: config.get('github.clientScrect'),
-}, (token, tokenSecret, profile, done) => {
-    database('users').where('githubId', profile.id).first().then(row => done(row))
+}, async function(token, tokenSecret, profile, done) {
+    const rows = await database('users').where('githubId', profile.id)
+
+    if (rows.length === 0) {
+        // create user
+        const user = await database('users').insert({
+            githubId: profile.id,
+            username: profile.username,
+            email: '',
+            password: '',
+        })
+
+        return (await database('users').where('id', user[0]))[0]
+    }
+    return done(null, rows[0])
 })
 
 passport.use(githubPassport)
