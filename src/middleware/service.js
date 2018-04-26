09cf69ccd0be6ba6e1ef78b417c09de667cb1aa8
Indexyz <jiduye@gmail.com>
2018-04-26 23:30:12 +0800

    feat: allow get config in view

--- src/middleware/service.js
+++ src/middleware/service.js
@@ -1,6 +1,8 @@
 import service from '../database/service'
+import config from 'config'
 
 export default async (ctx, next) => {
     ctx.service = service
+    ctx.state.config = config
     await next()
 }
