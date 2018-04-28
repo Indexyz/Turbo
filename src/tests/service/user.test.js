7017fb4e565b8faf344821c5b2c50099c2332919
Indexyz <jiduye@gmail.com>
2018-04-28 09:56:03 +0800

    test(service): add login test

--- src/tests/service/user.test.js
+++ src/tests/service/user.test.js
@@ -2,29 +2,46 @@ import should from 'should'
 import userService from '../../database/service/user'
 import database from '../../database/instance'
 
+let user = null
 
-const getTestUser = async () => {
-    return await userService.create({
+beforeEach(async () => {
+    user = await userService.create({
         username: 'indexyz',
         password: 'password',
         email: 'indexyz@email.com',
     })
-}
+})
 
-it('Create user', async () => {
-    const user = await getTestUser()
-
-    should(user).be.ok()
+afterEach(async () => {
     await database('users').delete()
         .where('id', user.id)
 })
 
-it('Test password hashed', async () => {
-    const user = await getTestUser()
+it('Create user', async () => {
+    should(user).be.ok()
+})
 
+it('Test password hashed', async () => {
     should(user).have.property('password')
     should(user.password).not.be.eql('password')
+})
 
-    await database('users').delete()
-        .where('id', user.id)
+describe('Login', async () => {
+    it('Test login with true password', async () => {
+        const loginUser = await userService.login('indexyz', 'password')
+
+        should(loginUser).be.eql(user)
+    })
+
+    it('Login with wrong password', async () => {
+        const loginUser = await userService.login('indexyz', 'anotherPassword')
+
+        should(loginUser).not.be.ok()
+    })
+
+    it('Login with wrong username', async () => {
+        const loginUser = await userService.login('username', 'password')
+
+        should(loginUser).not.be.ok()
+    })
 })
